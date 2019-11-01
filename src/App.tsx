e27424b552094946ac58ada4c32ac2cce84fab7f
ezequielzacca <ezequielzacca@gmail.com>
2019-11-01 10:19:20 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import frog from './icons/frograin.png';
 import './App.css';
+const randomBetween = (min: number, max: number) => Math.floor(Math.random() * (max - min) + min)
+const icons: Array<{ name: string }> = [{
+  name: "frograin"
+}, {
+  name: "varg"
+}]
+const twitch = window.Twitch.ext;
+const Icon: React.SFC<{ name: string }> = (props) => {
+  return (
+    <div className="icon-container">
+      <img className="action-flag" src="/miscs/flag.png" />
+      <img className="action-icon" src={`/icons/${props.name}.png`} />
+      <span className="counter">{randomBetween(1, 25)}</span>
+    </div>
+  )
 
+}
 const App: React.FC = () => {
+  let token: string = "";
+
+  twitch.onContext((context) => console.log("context: ", context))
+  twitch.onAuthorized((auth) => token = auth.token)
+  twitch.listen('broadcast', (target, contentType, data) => {
+    console.log('Received broadcast ', JSON.parse(data));
+
+  });
+
+  const sendDataToServer = async (e: any) => {
+    try {
+      await fetch("https://14b2278f.ngrok.io/frog", {
+        method: 'GET',
+        headers: new Headers({
+          'Authorization': 'Bearer ' + token,
+          'Content-Type': 'application/x-www-form-urlencoded'
+        })
+      })
+
+    } catch (e) {
+      console.log(e)
+    }
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Icon name="frograin" />
+        <Icon name="varg" />
+        <Icon name="nightmares" />
+        <Icon name="treeguard" />
       </header>
     </div>
   );
